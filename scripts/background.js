39e0a4f3f3408801b3427d9e0d9ae74cbbb21501
Ingmar Kole <ikole@student.codam.nl>
2021-04-14 16:22:27 +0200

    FINALLYYYYYYYYY i was making shit just wayy to complicated but this thankfully works :)

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,10 @@
-// console.log('im here!, eyyyy lmaoooooo');
-chrome.tabs.onActivated.addListener(tab => {
-	chrome.tabs.get(tab.tabId, current_tab_info => {
-		chrome.tabs.onUpdated.addListener(function (changeInfo, tab) {
-			if (tab.status == 'complete') {
-				if (/^https:\/\/4anime.to/.test(current_tab_info.url)) {
-					chrome.tabs.executeScript(null, {file: './scripts/foreground.js'}, () => console.log('Foreground script injected'))
-				}
-			}
-		});
-	});
-});
 
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+	if (changeInfo.status == 'complete' && tab.active) {
+		console.log(tab.url);
+		if (/^https:\/\/4anime.to/.test(tab.url)) {
+			chrome.tabs.executeScript(null, {file: './scripts/foreground.js'}, () => console.log('Foreground script injected'))
+	  }
+	}
+  })
+			
