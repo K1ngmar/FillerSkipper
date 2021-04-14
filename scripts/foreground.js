b59e6af7c8687a4bdf5cbb5b9f9f24904711a980
Ingmar Kole <ikole@student.codam.nl>
2021-04-14 17:31:11 +0200

    trying to parse the local json file but failing miserably

--- scripts/foreground.js
+++ scripts/foreground.js
@@ -5,3 +5,8 @@ function getShow() {
 }
 
 console.log(getShow());
+
+var file	= new XMLHttpRequest();
+var url		= chrome.runtime.getURL("./data/fillers.json")
+var my_JSON_object = JSON.parse(file.responseText);
+console.log(my_JSON_object.result[0]);
