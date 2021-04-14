ad5e024f9ccaffd83d26ac3f069ee937332178a2
Ingmar Kole <ikole@student.codam.nl>
2021-04-14 16:28:24 +0200

    cleaning like Idris

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,8 @@
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 	if (changeInfo.status == 'complete' && tab.active) {
-		console.log(tab.url);
 		if (/^https:\/\/4anime.to/.test(tab.url)) {
 			chrome.tabs.executeScript(null, {file: './scripts/foreground.js'}, () => console.log('Foreground script injected'))
-	  }
+		}
 	}
-  })
-			
+})			
--- scripts/foreground.js
+++ scripts/foreground.js
@@ -1,8 +1,7 @@
-//get show
 
 function getShow() {
 		var show = document.getElementById('titleleft').getAttribute('title');
 	return show;
 }
 
-console.log('i injected');
+console.log(getShow());
