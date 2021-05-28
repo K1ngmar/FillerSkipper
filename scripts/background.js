b8bd7ac6eda9f96839615dc450d31a3694392684
Ingmar Kole <ikole@student.codam.nl>
2021-05-28 16:43:06 +0200

    I honestly have no clue what im doing

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,10 @@
 
+
+
+function	skipEpisode(data) {
+	console.log(data);
+}
+
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 	if (changeInfo.status == 'complete' && tab.active) {
 		if (/^https:\/\/4anime.to/.test(tab.url)) {
@@ -9,8 +15,9 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 					if (!response.ok) {
 						throw new Error("HTTP error " + response.status);
 					}
-					console.log(response.json());
+					skipEpisode(response.json());
 				})
+
 			chrome.tabs.executeScript(null, {file: './scripts/foreground.js'}, () => console.log('Foreground script injected'))
 		}
 	}
