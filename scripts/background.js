635abca065baae8df48173a261ca72b9fc398382
Ingmar Kole <ikole@student.codam.nl>
2021-05-28 16:20:34 +0200

    Feat: Finally able to read the json file omg

--- scripts/background.js
+++ scripts/background.js
@@ -2,6 +2,15 @@
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 	if (changeInfo.status == 'complete' && tab.active) {
 		if (/^https:\/\/4anime.to/.test(tab.url)) {
+			var url		= chrome.runtime.getURL("./data/fillers.json")
+			console.log(url);
+			fetch(url)
+				.then(response => {
+					if (!response.ok) {
+						throw new Error("HTTP error " + response.status);
+					}
+					console.log(response.json());
+				})
 			chrome.tabs.executeScript(null, {file: './scripts/foreground.js'}, () => console.log('Foreground script injected'))
 		}
 	}
--- scripts/foreground.js
+++ scripts/foreground.js
@@ -1,12 +1,10 @@
 
 function getShow() {
-		var show = document.getElementById('titleleft').getAttribute('title');
+	var element = document.getElementById('titleleft');
+	if (element == null)
+		return null;
+	var show = element.getAttribute('title');
 	return show;
 }
 
 console.log(getShow());
-
-var file	= new XMLHttpRequest();
-var url		= chrome.runtime.getURL("./data/fillers.json")
-var my_JSON_object = JSON.parse(file.responseText);
-console.log(my_JSON_object.result[0]);
